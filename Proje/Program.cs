7f1cb131239e34e48d51398b7b17e5ddc465c7e9
Mehmet Bedir SEVIMLI <mehmetbedirsevimli123@gmail.com>
2023-12-28 21:03:59 +0300

    builder.Services.AddIdentity<ApplicationUser, IdentityRole>() ile Identity servislerini ekledim.

--- Proje/Program.cs
+++ Proje/Program.cs
@@ -4,8 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Proje.Data;
 
-using Proje.Data;
-
+var builder = WebApplication.CreateBuilder(args);
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
