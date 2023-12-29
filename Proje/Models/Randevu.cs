fc047c35d86c562ff87cb6ed2935e58306758e75
Mehmet Bedir SEVIMLI <mehmetbedirsevimli123@gmail.com>
2023-12-29 22:46:05 +0300

    Veritabanı nesneleri güncellendi.

--- Proje/Models/Randevu.cs
+++ Proje/Models/Randevu.cs
@@ -5,7 +5,7 @@
         public int RandevuId { get; set; }
         public int KullaniciId { get; set; }
         public int DoktorId { get; set; }
-        public int PoliklinikId { get; set; }
+        public int? PoliklinikId { get; set; }
         public DateTime Tarih { get; set; }
         public string? Durum { get; set; }
         public Kullanici? Kullanici { get; set; }
