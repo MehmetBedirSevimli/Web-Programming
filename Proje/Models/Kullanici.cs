bc56d53c7731a0ca07e7d97008b84c56fd42aaad
Mehmet Bedir SEVIMLI <mehmetbedirsevimli123@gmail.com>
2023-12-30 22:23:24 +0300

    ApplicationRole ve IdentityDbContext eklendi

--- Proje/Models/Kullanici.cs
+++ Proje/Models/Kullanici.cs
@@ -5,7 +5,5 @@
         public int KullaniciId { get; set; }
         public string Adi { get; set; }
         public string Soyadi { get; set; }
-        public string Email { get; set; }
-        public string Sifre { get; set; }
     }
 }
